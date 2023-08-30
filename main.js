f37708b36373baf73fcf69c0a5c7706b1b594265
do-hyun,kim <dohyun911@hanmail.net>
2023-08-30 10:05:00 +0900

    기본 자료형과 배열타입 자료형 타입 지정

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-const plus = (n1, n2) => {
-    console.log(n1 + n2);
-    return n1 + n2;
-};
-const result = plus(2, 3);
-console.log(result);
+//기본 타입
+let test = 'hello';
+let num = 0;
+let isFemale = true;
+//참조형 타입
+let like = ['game', 'music', 'movie'];
+let dislike = ['study', 'homework', 'exercise'];
+let odd = [1, 3, 5];
+let even = [2, 4, 6];
+let abc = [true, false, true];
+let bbc = [true, false, true];
+//tuple : 배열에 들어가는 자료형이 다를때 배열의 순서마다 타입지정
+let arr1 = [3, '3'];
--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
-const plus = (n1: number, n2: number): number => {
-	console.log(n1 + n2);
-	return n1 + n2;
-};
+//기본 타입
+let test: string = 'hello';
+let num: number = 0;
+let isFemale: boolean = true;
 
-const result = plus(2, 3);
-console.log(result);
+//참조형 타입
+let like: string[] = ['game', 'music', 'movie'];
+let dislike: Array<string> = ['study', 'homework', 'exercise'];
+let odd: number[] = [1, 3, 5];
+let even: Array<number> = [2, 4, 6];
+let abc: boolean[] = [true, false, true];
+let bbc: Array<boolean> = [true, false, true];
+//tuple : 배열에 들어가는 자료형이 다를때 배열의 순서마다 타입지정
+let arr1: [number, string] = [3, '3'];
