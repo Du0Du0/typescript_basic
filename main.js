a159e9aaaac61e21e70f4088b7b60ca2b392937b
do-hyun,kim <dohyun911@hanmail.net>
2023-08-30 10:00:00 +0900

    init

---
+++ main.js
@@ -0,0 +1,6 @@
+const plus = (n1, n2) => {
+    console.log(n1 + n2);
+    return n1 + n2;
+};
+const result = plus(2, 3);
+console.log(result);
---
+++ main.ts
@@ -0,0 +1,7 @@
+const plus = (n1: number, n2: number): number => {
+	console.log(n1 + n2);
+	return n1 + n2;
+};
+
+const result = plus(2, 3);
+console.log(result);
