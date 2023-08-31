f50fb3df12e13c7126a3ae1d584edc7c41d341bd
do-hyun,kim <dohyun911@hanmail.net>
2023-08-31 10:30:00 +0900

    인터페이스 추가 속성

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 let test = 'hello';
 let num = 0;
 let isFemale = true;
+
 //참조형 타입
 let like = ['game', 'music', 'movie'];
 let dislike = ['study', 'homework', 'exercise'];
@@ -9,5 +10,14 @@ let odd = [1, 3, 5];
 let even = [2, 4, 6];
 let abc = [true, false, true];
 let bbc = [true, false, true];
+
 //tuple : 배열에 들어가는 자료형이 다를때 배열의 순서마다 타입지정
 let arr1 = [3, '3'];
+
+//Student라는 커스텀 인터페이스 타입이 지정된 객체
+let student1 = {
+	name: 'David',
+	age: 20,
+	isFemale: false,
+	//address: 'Seoul',
+};
--- main.ts
+++ main.ts
@@ -12,3 +12,20 @@ let abc: boolean[] = [true, false, true];
 let bbc: Array<boolean> = [true, false, true];
 //tuple : 배열에 들어가는 자료형이 다를때 배열의 순서마다 타입지정
 let arr1: [number, string] = [3, '3'];
+
+//객체 타입지정
+//interface: 객체처럼 자료의 구조가 복잡한 경우 직접 커스터마이징된 타입을 생성
+interface Student {
+	name: string;
+	age: number;
+	readonly isFemale: boolean; //해당 property는 읽기 전용으로 변경 불가
+	address?: string; //해당 property는 optional하게 적용
+}
+
+//Student라는 커스텀 인터페이스 타입이 지정된 객체
+let student1: Student = {
+	name: 'David',
+	age: 20,
+	isFemale: false,
+	//address: 'Seoul',
+};
